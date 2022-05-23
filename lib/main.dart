f4108fdde2e8a99a5bbb8a3990699944ee091d1f
KONICCO <nico678514@gmail.com>
2022-05-23 20:07:22 +0700

    daftar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:bisa/buah.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'splash_screen.dart';
 import 'kategori.dart';
-
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
+DatabaseReference UsersRef = FirebaseDatabase.instance.ref().child("users");
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
