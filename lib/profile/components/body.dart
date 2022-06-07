7f010267b5b33d0ead6c6b69531468c66ab0ab24
Fikri Ariqsya <fikriariqsyarahman98@gmail.com>
2022-06-07 23:20:54 +0700

    add ui profil user

--- lib/profile/components/body.dart
+++ lib/profile/components/body.dart
@@ -2,6 +2,7 @@ import 'package:bisa/controllers/auth_controller.dart';
 import 'package:bisa/login_screen.dart';
 import 'package:bisa/map.dart';
 import 'package:bisa/notifikasi.dart';
+import 'package:bisa/profilUser.dart';
 import 'package:bisa/ubahProfil.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,7 @@ class Body extends StatelessWidget {
             press: () => {
               Navigator.push(context,
                   MaterialPageRoute(builder: (BuildContext ctx) {
-                return UbahProfil();
+                return profilUser();
               }))
             },
           ),
@@ -57,16 +58,17 @@ class Body extends StatelessWidget {
             icon: "images/icon/Log_out.svg",
             press: () {
               logout(context);
-            // Navigator.pushAndRemoveUntil(
-            // context,
-            // MaterialPageRoute(builder: (context) => LoginScreen()),
-            // (Route<dynamic> route) => false,);
-            } ,
+              // Navigator.pushAndRemoveUntil(
+              // context,
+              // MaterialPageRoute(builder: (context) => LoginScreen()),
+              // (Route<dynamic> route) => false,);
+            },
           ),
         ],
       ),
     );
   }
+
   Future<void> logout(BuildContext context) async {
     CircularProgressIndicator();
     await FirebaseAuth.instance.signOut();
